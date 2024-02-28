40d4c252f1d4c17935794d5ee161b319b5e3eae4
Geraldine Cassie <cassiemakaza447@gmail.com>
2024-02-28 11:50:49 +0200

    Stil yet to complete this challenge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_with
+  end
 end
